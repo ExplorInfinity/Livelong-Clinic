af8c84a025607f4aa45d49a8e39bd6a1d393fb0e
Aditya Singla <exploringhobby@gmail.com>
2025-11-26 13:53:31 +0530

    Added another prop to Doctor Card

--- src/app/features/findDoctor/doctorCard/doctorCard.tsx
+++ src/app/features/findDoctor/doctorCard/doctorCard.tsx
@@ -10,22 +10,23 @@ export type DoctorCardProps = {
     totalRatings: number
     totalReviews: number
     specialities: string[]
+    expelled?: boolean
 }
 
 const starTypes: string[] = ["★", "☆"];
 
 export default function DoctorCard({ 
-    imgsrc = unknown, name, degrees, rating, totalRatings, totalReviews, specialities
+    imgsrc = unknown, name, degrees, rating, totalRatings, totalReviews, specialities, expelled = false
 } : DoctorCardProps): JSX.Element {
 
-    const specialitiesList: Array<JSX.Element> = specialities.map(s => <li>{s}</li>);
+    const specialitiesList: Array<JSX.Element> = specialities.map((s: string): JSX.Element => <li>{s}</li>);
 
     return (
         <div className={styles.results}>
             <div className={styles.doctorCard}>
                 <img src={imgsrc} alt="Photo" />
                 <div className={styles.details}>
-                    <p className={styles.name}>{name}{...degrees.map(d => ", " + d)}</p>
+                    <p className={styles.name}>{name}{...degrees.map(d => ", " + d)} { expelled && <span className={styles.expelled}>Expelled</span> }</p>
                     <p className={styles.rating}>
                         <span className={styles.ratingStars}>{starTypes[0].repeat(Math.round(rating))}</span> 
                         {starTypes[1].repeat(5 - Math.round(rating))} {rating} of 5 stars | <span>{totalRatings} Ratings, {totalReviews} Reviews</span>
